4e0b9c4831b55c759b8cf2f39f514914630962fd
Peter Anastasov <peterdiman7@Peters-MacBook-Air.local>
2022-10-08 13:15:50 +0300

    react SPA

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import { Route, Switch } from 'react-router-dom';
+
+import Header from './components/Header/Header';
+import Footer from './components/Footer/Footer';
+import Categories from './components/Categories/Categories';
+import PetDetails from './components/PetDetails/PetDetails';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+        <Header />
+
+		<Switch>
+			<Route path="/" exact component={Categories} />
+			<Route path="/categories/:category" component={Categories} />
+			<Route path='/pets/details/:petId' component={PetDetails}/>
+		</Switch>
+
+        <Footer />
+    </div>
+  );
+}
+
+export default App;
